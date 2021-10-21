afc97c9ba42f29fd053a2062a233bc5684667abd
yunkai-zhang <59794765+yunkai-zhang@users.noreply.github.com>
2021-10-21 22:34:19 +0800

    更新springcloud学习的项目

--- small_projects_when_learning_java_backend/springcloud/springcloud-provider-dept-8001/src/main/java/com/zhangyun/springcloud/DeptProvider_8001.java
+++ small_projects_when_learning_java_backend/springcloud/springcloud-provider-dept-8001/src/main/java/com/zhangyun/springcloud/DeptProvider_8001.java
@@ -2,7 +2,14 @@ package com.zhangyun.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/*Eureka是C-S架构，主启动类添加该注解后，在子module启动后会自动注册本module的服务到Eureka中
+*
+* 可能有人会问“提供服务不应该用server吗？怎么用client？”。答：服务的提供者和消费者在Eureka C-S架构中
+* 都属于客户端client，两client都需要与服务端server（Eureka-server）交互。
+* */
+@EnableEurekaClient
 //@SpringBootApplication注解让当前类编程子module的主启动类
 @SpringBootApplication
 public class DeptProvider_8001 {
